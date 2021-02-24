97832b9c75ddeedce819c4e5c22f252d808f6c5f
Emilio Granell <egranell@sciling.com>
2021-02-24 14:18:46 +0100

    Initial kubeflow k8s deployment

--- stacks/kubeflow-mlflow/k8s/main.tf
+++ stacks/kubeflow-mlflow/k8s/main.tf
@@ -1,10 +1,52 @@
+terraform {
+  required_providers {
+    k8s = {
+      version = ">= 0.8.0"
+      source  = "banzaicloud/k8s"
+    }
+    kubernetes = {
+      version = "= 1.13.3"
+    }
+  }
+}
+
 provider "kubernetes" {
-  config_path    = "./kubeconfig"
+  config_path    = "/root/.kube/config"
   config_context = "minikube"
+}
+
+provider "k8s" {
+  config_path    = "/root/.kube/config"
+  config_context = "minikube"
+}
+
+provider "helm" {
+  kubernetes {
+   config_path    = "/root/.kube/config"
+    config_context = "minikube"
+ }
 }
 
 resource "kubernetes_namespace" "example2" {
   metadata {
     name = "hello-luke"
   }
+}
+module "kubeflow" {
+  providers = {
+    kubernetes = kubernetes
+    k8s        = k8s
+    helm       = helm
+  }
+
+  source  = "datarootsio/kubeflow/module"
+  version = "~>0.12"
+
+  #ingress_gateway_ip  = "10.20.30.40"
+  use_cert_manager    = true
+  install_istio        = true
+  install_cert_manager = true
+  domain_name         = "foo.local"
+  letsencrypt_email   = "foo@bar.local"
+  kubeflow_components = ["pipelines"]
 }
