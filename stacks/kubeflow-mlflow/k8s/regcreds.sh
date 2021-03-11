de9b62eb50437a534ea3c158dd9f1b9b7e68f2d8
Luke Marsden <me@lukemarsden.net>
2021-03-11 08:15:26 +0000

    fixups

--- stacks/kubeflow-mlflow/k8s/regcreds.sh
+++ stacks/kubeflow-mlflow/k8s/regcreds.sh
@@ -1,25 +1,99 @@
 #!/bin/bash
 set -euo pipefail
-for NS in \
-    admin \
-    auth \
-    cert-manager \
-    default \
-    istio-operator \
-    istio-system \
-    kf \
-    knative-serving \
-    kube-node-lease \
-    kube-public \
-    kube-system \
-    kubeflow \
-    kubeflow-operator; do
+
+NS_AND_SA="auth,default
+auth,dex
+cert-manager,cert-manager
+cert-manager,cert-manager-cainjector
+cert-manager,cert-manager-webhook
+cert-manager,default
+default,default
+istio-operator,default
+istio-operator,istio-operator
+istio-system,cluster-local-gateway-service-account
+istio-system,default
+istio-system,istio-ingressgateway-service-account
+istio-system,istio-reader-service-account
+istio-system,istiod-service-account
+istio-system,kiali-service-account
+istio-system,prometheus
+kf,default
+kf,minio
+kf,minio-update-prometheus-secret
+kf,mlflow
+kf,mysql
+knative-serving,controller
+knative-serving,default
+kube-node-lease,default
+kube-public,default
+kube-system,attachdetach-controller
+kube-system,bootstrap-signer
+kube-system,certificate-controller
+kube-system,clusterrole-aggregation-controller
+kube-system,coredns
+kube-system,cronjob-controller
+kube-system,daemon-set-controller
+kube-system,default
+kube-system,deployment-controller
+kube-system,disruption-controller
+kube-system,endpoint-controller
+kube-system,expand-controller
+kube-system,generic-garbage-collector
+kube-system,horizontal-pod-autoscaler
+kube-system,job-controller
+kube-system,kube-proxy
+kube-system,namespace-controller
+kube-system,node-controller
+kube-system,persistent-volume-binder
+kube-system,pod-garbage-collector
+kube-system,pv-protection-controller
+kube-system,pvc-protection-controller
+kube-system,replicaset-controller
+kube-system,replication-controller
+kube-system,resourcequota-controller
+kube-system,service-account-controller
+kube-system,service-controller
+kube-system,statefulset-controller
+kube-system,storage-provisioner
+kube-system,token-cleaner
+kube-system,ttl-controller
+kubeflow-operator,default
+kubeflow-operator,kubeflow-operator
+kubeflow,admission-webhook-service-account
+kubeflow,application-controller-service-account
+kubeflow,argo
+kubeflow,argo-ui
+kubeflow,centraldashboard
+kubeflow,default
+kubeflow,jupyter-web-app-service-account
+kubeflow,katib-controller
+kubeflow,katib-ui
+kubeflow,metadata-ui
+kubeflow,ml-pipeline
+kubeflow,ml-pipeline-persistenceagent
+kubeflow,ml-pipeline-scheduledworkflow
+kubeflow,ml-pipeline-ui
+kubeflow,ml-pipeline-viewer-crd-service-account
+kubeflow,notebook-controller-service-account
+kubeflow,pipeline-runner
+kubeflow,profiles-controller-service-account
+kubeflow,pytorch-operator
+kubeflow,seldon-manager
+kubeflow,spark-operatoroperator-sa
+kubeflow,spark-operatorspark
+kubeflow,spartakus
+kubeflow,tf-job-dashboard
+kubeflow,tf-job-operator"
+
+for NS_SA in $NS_AND_SA ; do
         (
+            NS=$(echo $NS_SA |cut -d , -f 0)
+            NS=$(echo $NS_SA |cut -d , -f 1)
             while ! kubectl get ns $NS 2>/dev/null; do
-                echo "waiting for ns $NS to be created..."
+                echo "waiting for ns $NS to be created so we can patch SA $SA in it..."
                 sleep 10
             done
             kubectl patch serviceaccount -n $NS \
-                default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
+                $SA -p '{"imagePullSecrets": [{"name": "regcred"}]}'
         ) &
 done
