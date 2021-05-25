59ad6ad68d14728a45b6ece83e3e4f37d86186b9
Luke Marsden <me@lukemarsden.net>
2021-05-25 12:00:32 +0100

    bump cluster size

--- eks.tf
+++ eks.tf
@@ -110,17 +110,17 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t3.small"
+      instance_type                 = "t3.large"
       additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 3
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t3.medium"
+      instance_type                 = "p3.2xlarge"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 3
+      asg_desired_capacity          = 0
     },
   ]
 
